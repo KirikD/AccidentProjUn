f914aa70f7b69c151109880d7691dff50b355c33
KirikD <fx3kd@yandex.ru>
2022-05-12 09:53:43 +0500

    новый день

---
+++ Assets/sdf.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sdf : MonoBehaviour
+{
+
+    void FuncA(string a)
+    {
+        
+    }
+
+    // 
+    void FuncA()
+    {
+        
+    }
+}
